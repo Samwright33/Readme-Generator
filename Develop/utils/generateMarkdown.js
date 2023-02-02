6e27d9716671f0b1be05678f7f04605d4f888cc4
Sam Wright <samwright@Sams-MacBook-Pro-3.local>
2023-02-02 00:38:38 -0600

    Added needed packages, wrote questions for user input, initialization, began markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,17 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Packages needed for application functionality 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Returns a license badge
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+  }
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +25,39 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+
+## Table of Contents 
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributiing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Description
+${data.descriton}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Submit any questions here
+Email: ${data.email}
+GitHub: https://github.com/${data.github}
 `;
 }
 
