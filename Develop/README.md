135bb7a7a8452d0f69e70b9c40be6ddd081aca70
Sam Wright <samwright@Sams-MacBook-Pro-3.local>
2023-02-02 23:44:06 -0600

    Completed generate read me function and adjusted questions

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+# er
+
+## Table of Contents 
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributiing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Description
+undefined
+
+## Installation
+er
+
+## Usage
+er
+
+## License
+MIT
+
+## Contributing
+undefined
+
+## Tests
+er
+
+## Questions
+Submit any questions here
+Email: er
+GitHub: https://github.com/undefined
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,101 @@
-// TODO: Include packages needed for this application
+// Packages needed for application functionality 
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Array of questions for user to answer
+const questions = [
+    {
+        type: 'input',
+        message: 'What would you like the title of the Readme to be called?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Write a description of the project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Provide installation instructions.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Describe how to use the application.',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license',
+        choices: ['MIT', 'CC-0', 'GNU', 'GPL'],
+        default: [0],
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Selecting a license is required');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Provide guidelines for user contributions.',
+        name: 'Contributing',
+    },
+    {
+        type: 'input',
+        message: 'How did you test the application?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'gitHubUser',
+    },
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+
+// function to write README file
+
+const writeToFile = data => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./README.md', data, err => {
+            if (err) {
+                reject (err);
+                return;
+            }
+            resolve({
+                ok: true
+            })
+        })
+    })
+}
+
+
+// function to initialize app
+const init = () => {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+.then(userInput => {
+    return generateMarkdown(userInput);
+})
+
+.then(Content => {
+    return writeToFile(Content);
+})
+
+.catch(err => {
+    console.log(err);
+});
